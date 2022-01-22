88e810ab26e91191b85c8c03d82ce411590f3175
Danilo55Amaral <danilo123amaral@gmail.com>
2022-01-22 18:23:42 -0300

    Alteração do dominio da api

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import QuestionModel from "../model/question";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://daniloteste.netlify.app/api';
 
 export default function Home() {
   const router = useRouter();
